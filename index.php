4254c56d19711e7403636da718d1be3c64ed1f1c
KeplerTahitix <69234657+KeplerTahitix@users.noreply.github.com>
2020-08-06 09:11:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,68 +1,41 @@
-<?php
-/**
- * Copyright 2018 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+<?php 
 
-// [START dialogflow_detect_intent_text]
-namespace Google\Cloud\Samples\Dialogflow;
+$method = $_SERVER['REQUEST_METHOD'];
 
-use Google\Cloud\Dialogflow\V2\SessionsClient;
-use Google\Cloud\Dialogflow\V2\TextInput;
-use Google\Cloud\Dialogflow\V2\QueryInput;
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
 
-/**
- * Returns the result of detect intent with texts as inputs.
- * Using the same `session_id` between requests allows continuation
- * of the conversation.
- */
-function detect_intent_texts($projectId, $texts, $sessionId, $languageCode = 'en-US')
-{
-    // new session
-    $sessionsClient = new SessionsClient();
-    $session = $sessionsClient->sessionName($projectId, $sessionId ?: uniqid());
-    printf('Session path: %s' . PHP_EOL, $session);
+	$text = $json->result->parameters->text;
 
-    // query for each string in array
-    foreach ($texts as $text) {
-        // create text input
-        $textInput = new TextInput();
-        $textInput->setText($text);
-        $textInput->setLanguageCode($languageCode);
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
 
-        // create query input
-        $queryInput = new QueryInput();
-        $queryInput->setText($textInput);
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
 
-        // get response and relevant info
-        $response = $sessionsClient->detectIntent($session, $queryInput);
-        $queryResult = $response->getQueryResult();
-        $queryText = $queryResult->getQueryText();
-        $intent = $queryResult->getIntent();
-        $displayName = $intent->getDisplayName();
-        $confidence = $queryResult->getIntentDetectionConfidence();
-        $fulfilmentText = $queryResult->getFulfillmentText();
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
 
-        // output relevant info
-        print(str_repeat("=", 20) . PHP_EOL);
-        printf('Query text: %s' . PHP_EOL, $queryText);
-        printf('Detected intent: %s (confidence: %f)' . PHP_EOL, $displayName,
-            $confidence);
-        print(PHP_EOL);
-        printf('Fulfilment text: %s' . PHP_EOL, $fulfilmentText);
-    }
-
-    $sessionsClient->close();
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
 }
-// [END dialogflow_detect_intent_text]
+else
+{
+	echo "Method not allowed";
+}
+
+?>
