41bb35550fdd745726a467e1072db8c6d316e875
KeplerTahitix <69234657+KeplerTahitix@users.noreply.github.com>
2020-08-06 11:32:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ if($method == 'POST'){
 	}
 
 	$response = new \stdClass();
-	$response->fulfillmentMessages = array((array($text));
+	$response->fulfillmentMessages = array($text);
 	echo json_encode($response);
 }
 else
